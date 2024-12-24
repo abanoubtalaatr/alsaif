d27db06bd335d4a434fe52563087955f8ad511df
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-12-24 21:37:05 +0200

    WIP

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BlogController;
+use App\Http\Controllers\Api\TrainingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('blogs', BlogController::class);
+Route::apiResource('trainings', TrainingController::class);
