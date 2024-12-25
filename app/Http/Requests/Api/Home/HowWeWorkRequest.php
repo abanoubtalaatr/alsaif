3b1156386984feac6b48098b649675d88b536f25
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-12-25 23:29:53 +0200

    WIP

--- app/Http/Requests/Api/Home/HowWeWorkRequest.php
+++ app/Http/Requests/Api/Home/HowWeWorkRequest.php
@@ -11,7 +11,7 @@ class HowWeWorkRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Api/Home/UpdateHowWeWorkRequest.php
+++ app/Http/Requests/Api/Home/UpdateHowWeWorkRequest.php
@@ -11,7 +11,7 @@ class UpdateHowWeWorkRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
