fe13de462e94468c5696c0c49cd40cbd5fe6e366
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-12-25 20:22:07 +0200

    WIP

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\Api\AboutUsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BlogController;
-use App\Http\Controllers\Api\ParagraphController;
+use App\Http\Controllers\Api\AboutUsController;
 use App\Http\Controllers\Api\TrainingController;
+use App\Http\Controllers\Api\AdvantageController;
+use App\Http\Controllers\Api\ParagraphController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,4 @@ Route::apiResource('trainings', TrainingController::class);
 Route::apiResource('about-us', AboutUsController::class)->only('index', 'store');
 Route::put('about-us', [AboutUsController::class, 'update']);
 Route::apiResource('paragraphs', ParagraphController::class)->only('store', 'update', 'destroy');
+Route::apiResource('advantages', AdvantageController::class);
