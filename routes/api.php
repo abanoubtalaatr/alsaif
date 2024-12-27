893184aec9644b71c2abe2bd9b6a9ed07e1a4d7d
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-12-27 17:26:58 +0200

    WIP

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BlogController;
 use App\Http\Controllers\Api\ClientController;
 use App\Http\Controllers\Api\AboutUsController;
@@ -20,43 +21,51 @@ use App\Http\Controllers\Api\Guide\ValueAddedTaxController;
 use App\Http\Controllers\Api\Home\HowWeWorkTitleController;
 use App\Http\Controllers\Api\Guide\FinancialModelController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('admin/login', [AuthController::class, 'login']);
 
-Route::apiResource('blogs', BlogController::class);
-Route::apiResource('trainings', TrainingController::class);
-Route::apiResource('about-us', AboutUsController::class)->only('index', 'store');
-Route::put('about-us', [AboutUsController::class, 'update']);
-Route::apiResource('paragraphs', ParagraphController::class)->only('store', 'update', 'destroy');
-Route::apiResource('advantages', AdvantageController::class);
-Route::apiResource('packages', PackageController::class);
+// Unauthenticated GET routes
+Route::get('blogs', [BlogController::class, 'index']);
+Route::get('trainings', [TrainingController::class, 'index']);
+Route::get('about-us', [AboutUsController::class, 'index']);
+Route::get('paragraphs', [ParagraphController::class, 'index']);
+Route::get('advantages', [AdvantageController::class, 'index']);
+Route::get('packages', [PackageController::class, 'index']);
+Route::get('financial-models', [FinancialModelController::class, 'index']);
+Route::get('pricings', [PricingController::class, 'index']);
+Route::get('value-added-taxes', [ValueAddedTaxController::class, 'index']);
+Route::get('sections', [SectionController::class, 'index']);
+Route::get('clients', [ClientController::class, 'index']);
+Route::get('testimonials', [TestimonialController::class, 'index']);
+Route::get('bookings', [BookingController::class, 'index']);
+Route::get('how-we-work-titles', [HowWeWorkTitleController::class, 'index']);
+Route::get('how-we-works', [HowWeWorkController::class, 'index']);
+Route::get('words', [WordController::class, 'index']);
+Route::get('statistics', [StatisticsController::class, 'index']);
 
-Route::apiResource('financial-models', FinancialModelController::class)->only('index', 'store');
-Route::put('financial-models', [FinancialModelController::class, 'update']);
-
-Route::apiResource('pricings', PricingController::class)->only('index', 'store');
-Route::put('pricings', [PricingController::class, 'update']);
-
-Route::apiResource('value-added-taxes', ValueAddedTaxController::class)->only('index', 'store');
-Route::put('value-added-taxes', [ValueAddedTaxController::class, 'update']);
-
-//Home page
-Route::apiResource('sections', SectionController::class)->only('index', 'store');
-Route::put('sections', [SectionController::class, 'update']);
-
-Route::apiResource('clients', ClientController::class);
-
-Route::apiResource('testimonials', TestimonialController::class);
-Route::apiResource('bookings', BookingController::class);
-
-Route::apiResource('how-we-work-titles', HowWeWorkTitleController::class)->only('index', 'store');
-Route::put('how-we-work-titles', [HowWeWorkTitleController::class, 'update']);
-
-
-Route::apiResource('how-we-works', HowWeWorkController::class);
-
-Route::apiResource('words', WordController::class)->only('index', 'store');
-Route::put('words', [WordController::class, 'update']);
-
-Route::apiResource('statistics', StatisticsController::class);
+// Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('blogs', BlogController::class)->except('index');
+    Route::apiResource('trainings', TrainingController::class)->except('index');
+    Route::apiResource('about-us', AboutUsController::class)->except('index');
+    Route::put('about-us', [AboutUsController::class, 'update']);
+    Route::apiResource('paragraphs', ParagraphController::class)->except('index');
+    Route::apiResource('advantages', AdvantageController::class)->except('index');
+    Route::apiResource('packages', PackageController::class)->except('index');
+    Route::apiResource('financial-models', FinancialModelController::class)->except('index');
+    Route::put('financial-models', [FinancialModelController::class, 'update']);
+    Route::apiResource('pricings', PricingController::class)->except('index');
+    Route::put('pricings', [PricingController::class, 'update']);
+    Route::apiResource('value-added-taxes', ValueAddedTaxController::class)->except('index');
+    Route::put('value-added-taxes', [ValueAddedTaxController::class, 'update']);
+    Route::apiResource('sections', SectionController::class)->except('index');
+    Route::put('sections', [SectionController::class, 'update']);
+    Route::apiResource('clients', ClientController::class)->except('index');
+    Route::apiResource('testimonials', TestimonialController::class)->except('index');
+    Route::apiResource('bookings', BookingController::class)->except('index');
+    Route::apiResource('how-we-work-titles', HowWeWorkTitleController::class)->except('index');
+    Route::put('how-we-work-titles', [HowWeWorkTitleController::class, 'update']);
+    Route::apiResource('how-we-works', HowWeWorkController::class)->except('index');
+    Route::apiResource('words', WordController::class)->except('index');
+    Route::put('words', [WordController::class, 'update']);
+    Route::apiResource('statistics', StatisticsController::class)->except('index');
+});
